8bbc51660905b88d8bd936de4ca633a46b1d4f77
Adam Bien <abien@adam-bien.com>
2015-12-11 11:09:24 +0100

    Gulp watching an executable file

--- gulp-sample/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp src/*.js /Users/abien/work/devprogs/tools/code-review/
--- gulp-sample/src/hey.js
+++ gulp-sample/src/hey.js
@@ -1,2 +1,4 @@
-print("hey ");
+#!/usr/bin/jjs -fv
+var java = $ENV.JAVA_HOME;
+print("hey from command line " + java);
 
